14020a468887caf1cdab233e4d3c7093f7ce2a60
marierose00 <dharma.marie.21@student.if.ittelkom-sby.ac.id>
2024-09-04 13:54:29 +0700

    Menambah halaman Admin dan edit routes

---
+++ app/Http/Controllers/AdminDataKaryawan.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminDataKaryawan extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('admin.data_karyawan.index', );
+    }
+    public function getDummyDataKaryawan()
+    {
+        return [
+            [
+                'id' => 1,
+                'nama' => 'toto',
+                'nik' => '111',
+                'jenis_kelamin' => 'laki laki',
+                'tempat_lahir' => 'inggris',
+                'tanggal_lahir' => '2024-08-01',
+                'alamat' => 'inggris',
+                'perusahaan' => 'perusahaan 3',
+                'posisi' => 'team principal',
+            ],
+            [
+                'id' => 2,
+                'nama' => 'titi',
+                'nik' => '111',
+                'jenis_kelamin' => 'laki laki',
+                'tempat_lahir' => 'inggris',
+                'tanggal_lahir' => '2024-08-01',
+                'alamat' => 'inggris',
+                'perusahaan' => 'mercedez',
+                'posisi' => 'team principal',
+            ],
+        ];
+    }
+
+    public function getDummyDataKaryawanJson()
+    {
+        $dummyData = $this->getDummyDataKaryawan();
+        return response()->json(['data' => $dummyData]);
+    }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+            // Fetch the dummy data
+        $dummyData = $this->getDummyDataKaryawan();
+
+        // dd($dummyData, $id);
+
+        // Find the specific data by ID
+        $data = collect($dummyData)->firstWhere('id', (int)$id);
+        if (is_array($data)) {
+            $data = (object) $data;
+        }
+        // dd($data);
+
+        if ($data) {
+            //  dd($data);
+            return view('admin.data_karyawan.edit', compact('data'));
+        } else {
+            return redirect()->back()->with('error', 'Data not found');
+        }
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use App\Http\Controllers\superAdminDashboard;
 use App\Http\Controllers\superAdminLaporanKehadiran;
 use App\Http\Controllers\superAdminPresensi;
 use App\Http\Controllers\superAdminDataKaryawan;
+
+use App\Http\Controllers\AdminDashboard;
+use App\Http\Controllers\AdminLaporanKehadiran;
+use App\Http\Controllers\AdminPresensi;
+use App\Http\Controllers\AdminDataKaryawan;
 use App\Http\Controllers\loginController;
 Route::get('/', function () {
     return view('welcome');
@@ -12,7 +17,7 @@ Route::get('/', function () {
 
 // Route::middleware('superadmin')->group(function () {
     Route::get('/SuperAdmin-dashboard', [superAdminDashboard::class, 'index'])->name('SA-dashboard');
-     Route::get('/SuperAdmin-LaporanKehadiran', [superAdminLaporanKehadiran::class, 'index'])->name('lapor.index');
+    Route::get('/SuperAdmin-LaporanKehadiran', [superAdminLaporanKehadiran::class, 'index'])->name('lapor.index');
     Route::get('/SuperAdmin-LaporanKehadiran-terlambat', [superAdminLaporanKehadiran::class, 'terlambat'])->name('lapor.terlambat');
     Route::get('/SuperAdmin-LaporanKehadiran-lembur', [superAdminLaporanKehadiran::class, 'lembur'])->name('lapor.lembur');
     Route::get('/SuperAdmin-Presensi', [superAdminPresensi::class, 'index'])->name('presensi.index');
@@ -24,6 +29,19 @@ Route::get('/', function () {
     Route::get('/SuperAdmin-LaporanKehadiran-dataKehadiran-terlambat', [superAdminLaporanKehadiran::class, 'getDummyTerlambat'])->name('Data_kehadiran.getTerlambat');
     Route::get('/SuperAdmin-LaporanKehadiran-dataKehadiran-lembur', [superAdminLaporanKehadiran::class, 'getDummyLembur'])->name('Data_kehadiran.getLembur');
     Route::get('/SuperAdmin-DataKaryawan-dataKaryawan', [superAdminDataKaryawan::class, 'getDummyDataKaryawanJson'])->name('Data_karyawan.getDummy');
+
+    Route::get('/Admin-dashboard', [AdminDashboard::class, 'index'])->name('Admin-dashboard');
+    Route::get('/Admin-LaporanKehadiran', [AdminLaporanKehadiran::class, 'index'])->name('lapor.index');
+    Route::get('/Admin-LaporanKehadiran-terlambat', [AdminLaporanKehadiran::class, 'terlambat'])->name('lapor.terlambat');
+    Route::get('/Admin-LaporanKehadiran-lembur', [AdminLaporanKehadiran::class, 'lembur'])->name('lapor.lembur');
+    Route::get('/Admin-Presensi', [AdminPresensi::class, 'index'])->name('presensi.index');
+    Route::get('/Admin-DataKaryawan', [AdminDataKaryawan::class, 'index'])->name('Data_karyawan.index');
+    Route::get('/Admin-DataKaryawan/{id}/edit', [AdminDataKaryawan::class, 'edit'])->name('Data_karyawan.edit');
+
+    Route::get('/Admin-LaporanKehadiran-dataKehadiran', [AdminLaporanKehadiran::class, 'getDummyKehadiran'])->name('Data_kehadiran.getDummy');
+    Route::get('/Admin-LaporanKehadiran-dataKehadiran-terlambat', [AdminLaporanKehadiran::class, 'getDummyTerlambat'])->name('Data_kehadiran.getTerlambat');
+    Route::get('/Admin-LaporanKehadiran-dataKehadiran-lembur', [AdminLaporanKehadiran::class, 'getDummyLembur'])->name('Data_kehadiran.getLembur');
+    Route::get('/Admin-DataKaryawan-dataKaryawan', [AdminDataKaryawan::class, 'getDummyDataKaryawanJson'])->name('Data_karyawan.getDummy');
 
 // });
 Route::get('/login', [loginController::class, 'form'])->name('login.form');
